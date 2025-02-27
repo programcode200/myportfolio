1647f763243cd475dfcfacb415d6d2962e7551ff
programcode200 <rohans1021@gmail.com>
2025-02-27 14:21:50 +0530

    add form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -25,33 +25,24 @@ const Contact = () => {
           <section className="w-full md:w-1/2 flex items-center">
             <div className="py-4 lg:pt-16 px-4 mx-auto w-full">
               <form 
-                name="contact" 
-                method="POST" 
+                name="contact v1" 
+                method="post" 
                 data-netlify="true"
-                data-netlify-honeypot="bot-field"
+                // data-netlify-honeypot="bot-field"
+                onSubmit="submit"
                 className="space-y-8"
               >
                 {/* Hidden Input for Netlify */}
                 <input type="hidden" name="form-name" value="contact" />
                 
-                {/* Honeypot Field (For spam protection) */}
-                <div hidden>
-                  <label>
-                    Don’t fill this out if you’re human: 
-                    <input name="bot-field" />
-                  </label>
-                </div>
-
                 <div>
                   <label
-                    htmlFor="email"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                   >
                     Your email
                   </label>
                   <input
                     type="email"
-                    id="email"
                     name="email"
                     className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-blue-500 dark:shadow-sm dark:shadow-slate-700"
                     placeholder="name@example.com"
@@ -60,14 +51,12 @@ const Contact = () => {
                 </div>
                 <div>
                   <label
-                    htmlFor="subject"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                   >
                     Subject
                   </label>
                   <input
                     type="text"
-                    id="subject"
                     name="subject"
                     className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                     placeholder="Let us know how we can help you"
@@ -76,13 +65,11 @@ const Contact = () => {
                 </div>
                 <div className="sm:col-span-2">
                   <label
-                    htmlFor="message"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
                   >
                     Your message
                   </label>
                   <textarea
-                    id="message"
                     name="message"
                     rows="6"
                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
